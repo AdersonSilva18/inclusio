ac3cded6d10ef28b5eaec9c99e47ce1657ada298
Aderson Silva <aderson.batista19@gmail.com>
2025-03-31 22:32:04 -0300

    ajust pdf

--- app/Http/Controllers/PdfGeneratorController.php
+++ app/Http/Controllers/PdfGeneratorController.php
@@ -16,7 +16,10 @@ class PdfGeneratorController extends Controller
             'nome' => "Document Ats ".date('Y-m-d H:i:s'),
         ]);
 
-        $pdf = Pdf::view('pdf.modelo-ats', $data)->save(storage_path('app/public/documentats'.$document->id.'.pdf'));
+        $pdf = Pdf::view('pdf.modelo-ats', $data)
+            ->withBrowsershot(function (Browsershot $browsershot) {
+            $browsershot->noSandbox();
+        })->save(storage_path('app/public/documentats'.$document->id.'.pdf'));
 
         if (Auth()->user()) {
             $document->user_id = Auth()->user()->id;
